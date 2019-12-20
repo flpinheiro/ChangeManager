4f2ee0f76c457fe0829ba6a357d0e1911bdac2b0
Felipe Luís Pinheiro <37678422+flpinheiro@users.noreply.github.com>
2019-12-20 13:21:18 -0300

    initial

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeManager.Domain
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Domain/Coin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeManager.Domain
+{
+    public class Coin: BaseEntity
+
+    {
+    }
+}
